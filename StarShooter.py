2707959aeea9699d6300f1da41616fbe5d4e9439
Romário Diogo <88805483+2506roma@users.noreply.github.com>
2025-03-17 20:26:49 -0300

    Update StarShooter.py

--- StarShooter.py
+++ StarShooter.py
@@ -12,5 +12,6 @@ while True:
     # Check for all events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
+            print("Quint...")
             pygame.quit()# Close Window
             quit()# End pygame
